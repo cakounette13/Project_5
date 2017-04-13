4a8548a5f72bac6b3623a604088ac049261f6dbe
carine <carinedelrieux@gmail.com>
2017-04-13 20:22:16 +0200

    Rectif test sur Email OK Modif vue recap clt OK

--- tests/Services/EmailTest.php
+++ tests/Services/EmailTest.php
@@ -1,58 +1,52 @@
 <?php
 namespace Tests\Services;
 
+use Doctrine\ORM\EntityManager;
 use OC\FideliteBundle\Entity\Client;
-use OC\FideliteBundle\Services\Email;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class EmailTest extends WebTestCase
 {
+    // test ne pouvant être fait qu'une fois
     /**
-     * @var Email
+     * @var EntityManager
      */
-    private $email;
-
-    /**
-     * @var ClientRepository
-     *
-     */
-    private $clientRepository;
+    private $em;
 
     public function setUp()
     {
-        $kernel = static::createKernel();
-        $kernel->boot();
-        $this->clientRepository = $kernel->getContainer()
-            ->get('doctrine.orm.entity_manager')
-            ->getRepository('OCFideliteBundle:Client');
-//        parent::setUp(); //
-//        $client = new Client();
-//        $client->setNom('DELRIEUX');
-//        $client->setPrenom('Carine');
-//        $client->setSociete('WEBB');
-//        $client->setCodePostal('06200');
-//        $client->setVille('Nice');
-//        $client->setPortable('0666666666');
-//        $client->setDateNaissance('NOW');
-//        $client->setEmail('c@gmail.com');
-//        $client->setMailEnvoyeLe('11/04/2017');
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $client = new Client();
+        $client->setDenomination('Madame');
+        $client->setNom('DELRIEUX');
+        $client->setPrenom('Carine');
+        $client->setSociete('WEB');
+        $client->setCodePostal('06200');
+        $client->setVille('Nice');
+        $client->setPortable('0666666664');
+        $client->setDateNaissance(\DateTime::createFromFormat('d/m/Y','13/04/2017')); // Mettre la date du jour du test
+        $client->setEmail('carine@gmail.com');
+        $client->setMailEnvoyeLe(\DateTime::createFromFormat('d/m/Y','30/03/2017'));
+        $this->em->persist($client);
+        $this->em->flush($client);
     }
 
     public function testEnvoiMailI()
     {
         $requete = static::createClient();
 
-        $clients = $this->clientRepository->findAll();
-        $client = $clients['0'];
-        $requete->getContainer()->get('oc_fidelite.email')->envoiMail($client);
+        $requete->enableProfiler();
 
-//        $requete->enableProfiler();
-//
-//        $crawler = $requete->request('GET', '/');
+       $crawler = $requete->request('GET', '/');
         $mailCollector = $requete->getProfile()->getCollector('swiftmailer');
 
         // Check that an email was sent
-        $this->assertEquals(1, $this->getMessageCount());
+        $this->assertEquals(1, $mailCollector->getMessageCount());
 
         $collectedMessages = $mailCollector->getMessages();
         $message = $collectedMessages[0];
@@ -61,6 +55,6 @@ class EmailTest extends WebTestCase
         $this->assertInstanceOf('Swift_Message', $message);
         $this->assertEquals('CAVE ... - Cadeau d\'anniversaire', $message->getSubject());
         $this->assertEquals('carinedelrieux@gmail.fr', key($message->getFrom()));
-        $this->assertEquals('carinedelrieux@gmail.com', key($message->getTo()));
+        $this->assertEquals('carine@gmail.com', key($message->getTo()));
     }
 }
